4ece71d7cd24a658db6e99843022a82f44b61e68
baralh <hugo.baralBTSSN@gmail.com>
2019-12-03 08:11:55 +0100

    Initial Commit

---
+++ Memo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by Hugo Baral on 03/12/2019.
+//  Copyright © 2019 Hugo Baral. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
