8dc611fcaf2c962573cbda22a95d892119c32874
baralh <hugo.baralBTSSN@gmail.com>
2019-12-04 11:17:51 +0100

    UI Page Note

--- Memo/NoteController.swift
+++ Memo/NoteController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class NoteController: UIViewController {
     
+    
+    @IBOutlet weak var UIContent: UITextView!
+    @IBOutlet weak var UITime: UILabel!
+    
+    @IBOutlet weak var UITodo: UIButton!
+    @IBOutlet weak var UIPicture: UIButton!
+    @IBOutlet weak var UIRecord: UIButton!
+    
     var note: Note?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(note)
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(shareTapped))
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(addTapped))
+//
+//        let share = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+//        let add = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
+//        navigationItem.rightBarButtonItems = [add, share]
+        
+        self.navigationItem.title = note?.title;
+        UIContent.text = note?.content
+        let format = DateFormatter()
+        format.dateFormat = "dd MMMM yyyy | HH:mm"
+        format.locale = Locale(identifier: "FR-fr")
+        UITime.text = format.string(from: (note?.date)!)
+    }
+    
+    @objc func addTapped(){
+        
+    }
+    
+    @objc func shareTapped(){
+        
     }
 }
