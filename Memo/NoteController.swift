646729d0d28eb0798932e205fdd34fb67152fd98
RyanBertrand1 <ryan.bertrand1@gmail.com>
2019-12-09 13:44:13 +0100

    update + save note

--- Memo/NoteController.swift
+++ Memo/NoteController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SQLite3
 
 
 class NoteController: UIViewController, UITextViewDelegate, UINavigationControllerDelegate, UIScrollViewDelegate {
@@ -24,6 +25,15 @@ class NoteController: UIViewController, UITextViewDelegate, UINavigationControll
     var note: Note?
     var barButton: UIBarButtonItem!
     var imagePicker: UIImagePickerController!
+    
+    var db: OpaquePointer?
+    
+    var titleView = UITextField()
+    
+    var titleModif = false
+    var contentModif = false
+    var newTitle: String = ""
+    var newContent: String = ""
 
     enum ImageSource {
          case photoLibrary
@@ -40,6 +50,8 @@ class NoteController: UIViewController, UITextViewDelegate, UINavigationControll
 //        let add = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
 //        navigationItem.rightBarButtonItems = [add, share]
         
+        initDB()
+        
         self.navigationItem.title = note?.title;
         UIContent.text = note?.content
         let format = DateFormatter()
@@ -51,6 +63,35 @@ class NoteController: UIViewController, UITextViewDelegate, UINavigationControll
         //btnDel.image  = UIImage(systemName: "return")
         UIContent.inputAccessoryView = TBMisc
         UIContent.delegate = self
+        
+        titleView.text = note?.title
+        titleView.frame = CGRect(x: 0, y: 0, width: 150, height: 1)
+        titleView.borderStyle = .none
+        titleView.layer.backgroundColor = UIColor.black.cgColor
+        navigationItem.titleView = titleView
+        titleView.addTarget(self, action: #selector(textFieldDidChange(_:)),
+        for: UIControl.Event.editingChanged)
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        newTitle = String(textField.text!)
+        print(newTitle)
+        
+        titleModif = true
+    }
+    
+    func initDB(){
+        let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent("NotesDatabase.sqlite")
+        
+        if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
+            print("error opening database")
+        }
+        
+        if sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS Notes (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT,content TEXT, date DATE)", nil, nil, nil) != SQLITE_OK {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("error creating table: \(errmsg)")
+        }
     }
     
     func textViewDidBeginEditing(_ textView: UITextView) {
@@ -168,6 +209,164 @@ class NoteController: UIViewController, UITextViewDelegate, UINavigationControll
           ac.addAction(UIAlertAction(title: "OK", style: .default))
           present(ac, animated: true)
       }
+    
+    func textViewDidChange(_ textView: UITextView) { //Handle the text changes here
+        newContent = UIContent.text!
+        if(newContent != note?.content){
+            contentModif = true
+        }
+        else{
+            contentModif = false
+        }
+    }
+    
+    func updateNote(){
+        var queryString: String
+        
+        var stmt: OpaquePointer?
+        
+        let id = Int(note!.id!)
+        
+        if(titleModif == true){
+            let dateFormatter: DateFormatter = {
+                let _formatter = DateFormatter()
+                _formatter.dateFormat = "dd MMMM yyyy | HH:mm"
+                _formatter.locale = Locale(identifier: "FR-fr")
+                return _formatter
+            }()
+            
+            let newDate = dateFormatter.string(from: Date())
+            
+            queryString = "UPDATE Notes SET title = '\(newTitle)',date = '\(newDate)'  WHERE Id = \(id)"
+            
+            if sqlite3_prepare(db, queryString, -1, &stmt, nil) != SQLITE_OK{
+                let errmsg = String(cString: sqlite3_errmsg(db)!)
+                print("error preparing insert: \(errmsg)")
+                return
+            }
+            
+            if sqlite3_step(stmt) != SQLITE_DONE {
+                let errmsg = String(cString: sqlite3_errmsg(db)!)
+                print("failure inserting notes: \(errmsg)")
+                return
+            }
+        }
+        
+        if(contentModif == true){
+            let dateFormatter: DateFormatter = {
+                let _formatter = DateFormatter()
+                _formatter.dateFormat = "dd MMMM yyyy | HH:mm"
+                _formatter.locale = Locale(identifier: "FR-fr")
+                return _formatter
+            }()
+            
+            let newDate = dateFormatter.string(from: Date())
+            
+            queryString = "UPDATE Notes SET content = '\(newContent)',date = '\(newDate)'  WHERE Id = \(id)"
+            
+            if sqlite3_prepare(db, queryString, -1, &stmt, nil) != SQLITE_OK{
+                let errmsg = String(cString: sqlite3_errmsg(db)!)
+                print("error preparing insert: \(errmsg)")
+                return
+            }
+            
+            if sqlite3_step(stmt) != SQLITE_DONE {
+                let errmsg = String(cString: sqlite3_errmsg(db)!)
+                print("failure inserting notes: \(errmsg)")
+                return
+            }
+        }
+        
+        contentModif = false
+        titleModif = false
+    }
+    
+    func saveNote(){
+        var stmt: OpaquePointer?
+        
+        //the insert query
+        let queryString = "INSERT INTO Notes (title, content, date) VALUES (?,?,?)"
+        
+        //preparing the query
+        if sqlite3_prepare(db, queryString, -1, &stmt, nil) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("error preparing insert: \(errmsg)")
+            return
+        }
+        
+        let title = newTitle
+        let content = newContent
+        
+        let dateFormatter: DateFormatter = {
+            let _formatter = DateFormatter()
+            _formatter.dateFormat = "dd MMMM yyyy | HH:mm"
+            _formatter.locale = Locale(identifier: "FR-fr")
+            return _formatter
+        }()
+        
+        let date = dateFormatter.string(from: Date())
+        
+        let SQLITE_TRANSIENT = unsafeBitCast(OpaquePointer(bitPattern: -1), to: sqlite3_destructor_type.self)
+        
+        if sqlite3_bind_text(stmt, 1, title, -1, SQLITE_TRANSIENT) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("failure binding title: \(errmsg)")
+            return
+        }
+        
+        if sqlite3_bind_text(stmt, 2, content, -1, SQLITE_TRANSIENT ) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("failure binding content: \(errmsg)")
+            return
+        }
+        
+        if sqlite3_bind_text(stmt, 3, date, -1, SQLITE_TRANSIENT) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("failure binding date: \(errmsg)")
+            return
+        }
+        
+        if sqlite3_step(stmt) != SQLITE_DONE {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("failure inserting notes: \(errmsg)")
+            return
+        }
+        
+        let queryString2 = "SELECT * FROM Notes WHERE title LIKE '\(newTitle)%' AND content LIKE '\(newContent)'"
+        
+        var stmt2:OpaquePointer?
+        
+        if sqlite3_prepare(db, queryString2, -1, &stmt2, nil) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("error preparing insert: \(errmsg)")
+            return
+        }
+        
+        if sqlite3_step(stmt2) == SQLITE_ROW {
+            let id = sqlite3_column_int(stmt, 0)
+            let title = String(cString: sqlite3_column_text(stmt, 1))
+            let content = String(cString: sqlite3_column_text(stmt, 2))
+            let stringDate = String(cString: sqlite3_column_text(stmt, 3))
+            let date = dateFormatter.date(from: stringDate);
+            
+            print(title)
+            
+            self.note = Note(id: Int(id), title: title, content: content, pictures: [], date: date!)
+            
+            print (self.note)
+        }
+        
+        
+    }
+    
+    @IBAction func valider(){
+        if (note?.id != nil){
+            updateNote()
+        }
+        else{
+            saveNote()
+        }
+    }
 }
 
 extension NoteController: UIImagePickerControllerDelegate{
